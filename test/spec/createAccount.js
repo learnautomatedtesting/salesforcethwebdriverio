8f544e47a861bd59935f916a1fa2a0a9c8d45c1f
Jeffrey Jongkees <jeffrey.jongkees@mn.nl>
2023-11-17 16:04:59 +0100

    All hard coded data removed from the testscript

--- test/spec/createAccount.js
+++ test/spec/createAccount.js
@@ -42,31 +42,42 @@ it('Creating an account', async ()=> {
   // Click the New button to create a new account
   await Account.click_NewButton();
 
-  await Account.fill_In_Accounts_Name_and_PhoneNumber(jsonData.input.account, jsonData.input.phonenumber1)
+  await Account.fill_In_Accounts_Name_and_PhoneNumber(jsonData.input.account, 
+                                                      jsonData.input.phonenumber1)
 
   // Fill in the new account's details
-  await Account.fill_In_Combo('Type', 'Prospect');
+  await Account.fill_In_Combo(jsonData.input.labelTypeOfAccount,
+                              jsonData.input.typeOfAccount);
 
   // Fill in the new account's details
-  await Account.fill_In_Combo('Industry', 'Banking');
+  await Account.fill_In_Combo(jsonData.input.labelTypeOfIndustry,
+                              jsonData.input.typeOfIndustry);
 
   // Fill in additional information
-  await Account.fill_In_additionalInformation(jsonData.input.website, jsonData.input.description,
-                                              jsonData.input.phonenumber2, jsonData.input.employees);
+  await Account.fill_In_additionalInformation(jsonData.input.website,
+                                              jsonData.input.description,
+                                              jsonData.input.phonenumber2,
+                                              jsonData.input.employees);
   
   // Fill in the address information
   // There are two types of addresses: BillingAddress and ShippingAddress
   // Here the BillingAddress is requiered
-  await Account.fill_In_AddressInformation('BillingAddress', jsonData.input.address, jsonData.input.postalCode, 
-                                            jsonData.input.city, jsonData.input.province,
-                                            jsonData.input.country);
+  await Account.fill_In_AddressInformation(jsonData.billingAddress.billingAddress, 
+                                           jsonData.billingAddress.address, 
+                                           jsonData.billingAddress.postalCode, 
+                                           jsonData.billingAddress.city,
+                                           jsonData.billingAddress.province,
+                                           jsonData.billingAddress.country);
 
   // Fill in the address information
   // There are two types of addresses: BillingAddress and ShippingAddress
   // Here the ShippingAddress is requiered
-  await Account.fill_In_AddressInformation('ShippingAddress', jsonData.input.address, jsonData.input.postalCode, 
-                                            jsonData.input.city, jsonData.input.province,
-                                            jsonData.input.country);
+  await Account.fill_In_AddressInformation(jsonData.shippingAddress.shippingAddress,
+                                           jsonData.shippingAddress.address,
+                                           jsonData.shippingAddress.postalCode, 
+                                           jsonData.shippingAddress.city,
+                                           jsonData.shippingAddress.province,
+                                           jsonData.shippingAddress.country);
 
 })
 
