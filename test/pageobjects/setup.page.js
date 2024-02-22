b05366973bb9008a74b3fb6aee43693b9a24020b
Jeffrey Jongkees <jeffreyjongkees@bsure-digital.nl>
2024-02-22 10:06:11 +0100

    test

--- test/pageobjects/setup.page.js
+++ test/pageobjects/setup.page.js
@@ -6,36 +6,29 @@ class Setup {
   };
 
   async click_AppLauncher() {
+
+    let contentPanel = await $('[class="panel-content scrollable"]');
+
     await this.elements.appLauncher().waitForExist();
     await this.elements.appLauncher().moveTo();
     await this.elements.appLauncher().waitForClickable();
     await this.elements.appLauncher().click();
-  }
 
-  //     async click_Service() {
-
-  //         await this.elements.service().waitForExist();
-  //         await this.elements.service().moveTo();
-  //         await this.elements.service().waitForClickable();
-  //         await this.elements.service().click();
-  //     }
-
-  // }
-
-  async click_Service() {
-    
-    let contentPanel = await $('[class="panel-content scrollable"]');
-
-    if (!contentPanel.isDisplayed()) {
-      await this.elements.appLauncher().click();
-    } else {
-      await this.elements.service().waitForExist();
-      await this.elements.service().moveTo();
-      await this.elements.service().waitForClickable();
-      await this.elements.service().click();
+    if(!contentPanel.isDisplayedInViewport()) {
+        await this.elements.appLauncher().click();
     }
   }
+
+      async click_Service() {
+
+          await this.elements.service().waitForExist();
+          await this.elements.service().moveTo();
+          await this.elements.service().waitForClickable();
+          await this.elements.service().click();
+      }
+
 }
+
 
 
 
