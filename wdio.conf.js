7d0269ef63cb522ee7750eca0fdf30ac4edede35
Jeffrey Jongkees <jeffreyjongkees@bsure-digital.nl>
2024-02-26 10:25:35 +0100

    push to learnautomated testing

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,7 +59,7 @@ export const config = {
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
-    //!!!!! ['--window-size=1920,1080', '--headless, --disable-gpu'] !!!!!
+    //!!!!!! ['--window-size=1920,1080', '--headless, --disable-gpu'] !!!!!!
     capabilities: [
       {
         browserName: browserName,
