c72e89946b39d47de5b130af7b31e63ee8ecd282
Jeffrey Jongkees <jeffreyjongkees@bsure-digital.nl>
2024-02-26 09:41:46 +0100

    test2 headlessmode

--- wdio.conf.js
+++ wdio.conf.js
@@ -66,12 +66,12 @@ export const config = {
 
         ...(browserName === 'chrome' ? {
           "goog:chromeOptions": {
-            args: ['--window-size=1920,1080', '--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage'],
+            args: ['--headless', '--disable-gpu'],
           },
         } : {}),
         ...(browserName === 'MicrosoftEdge' ? {
           "ms:edgeOptions": {
-           args: ['--window-size=1920,1080', '--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage'],
+           args: ['--headless', '--disable-gpu'],
           },
         } : {}), 
       }
